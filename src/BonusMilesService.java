2acf0c83c55754460cb868b9647e0978cc695812
Alexeuss82 <krukovalex@gmail.com>
2022-10-21 14:55:33 +0300

    удаление черновика кода из 1 задания

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -5,8 +5,3 @@ public class BonusMilesService {
     }
 
 }
-// public static void main(String[] args) {
-// int amountTicket = 1000;
-// int oneBonusMill = amountTicket / 20;
-//  int ticketBonusMills = amountTicket / oneBonusMill;
-// System.out.println("Вам начислено " + ticketBonusMills + " бонусных милль!");
